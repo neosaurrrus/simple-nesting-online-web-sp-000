d50932590dcee4b0edcdc339cafc6d7f16ec36c6
Lukie Kang <lukiek+f@gmail.com>
2019-05-29 22:19:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,7 +79,11 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
+        },
+				programmer_hash[:yukihiro_matsumoto] = {
+	 		       known_for: "Ruby",
+	 		    	 languages: ["LISP", "C"]
+	 				 }
      }
 
 		 programmer_hash[:yukihiro_matsumoto] = {
